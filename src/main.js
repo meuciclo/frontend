543342880c2411d874ebb938cd09ed47e8d0398a
Gabriel <gabriel.suriano@usp.br>
2021-05-18 14:00:56 -0300

    [FEAT] Installs maska

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import maska from 'maska'
 
 const app = createApp(App)
 app.use(router)
+app.use(maska)
 app.mount('#app')
